ed43b968a671a0afbf87fe178333007e5ba5c645
Ge0h <george.walker.wme@gmail.com>
2021-12-23 14:10:01 +0000

    Mobile friendly added

--- src/Components/MemoryCard.js
+++ src/Components/MemoryCard.js
@@ -1,9 +1,8 @@
 const MemoryCard = ({ image, onInc }) => {
     return (
-        <div onClick={() => onInc(image.id)} className='card my-2 py-1-2'>
+        <div onClick={() => onInc(image.id)} className='card flex my-1 py-1-2'>
             <div className='container flex'>
                 <img src={image.src} alt={image.charname}></img>
-                <h1>{image.charname}</h1>
             </div>
         </div>
     )
